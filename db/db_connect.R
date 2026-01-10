22911c6a32007ce123a39011d3059fa748050cc2
Seph Solomo <solomojoseph051@gmail.com>
2026-01-10 12:05:45 +0800

    Fix database connection logic for Render

--- db/db_connect.R
+++ db/db_connect.R
@@ -1,12 +1,27 @@
 library(DBI)
 library(RPostgres)
 
-# This will use Render's settings if available, otherwise it uses your local settings
-db_conn <- dbConnect(
-  RPostgres::Postgres(),
-  dbname   = Sys.getenv("DB_NAME", "healthcenter"),
-  host     = Sys.getenv("DB_HOST", "localhost"),
-  port     = Sys.getenv("DB_PORT", "5432"),
-  user     = Sys.getenv("DB_USER", "postgres"),
-  password = Sys.getenv("DB_PASS", "your_local_password") 
-)
+# Check if we are on Render by looking for a Render-specific variable
+is_render <- Sys.getenv("RENDER") != ""
+
+if (is_render) {
+  # RENDER CONNECTION (Cloud)
+  db_conn <- dbConnect(
+    RPostgres::Postgres(),
+    dbname   = Sys.getenv("DB_NAME"),
+    host     = Sys.getenv("DB_HOST"),
+    port     = as.integer(Sys.getenv("DB_PORT")),
+    user     = Sys.getenv("DB_USER"),
+    password = Sys.getenv("DB_PASS")
+  )
+} else {
+  # LOCAL CONNECTION (Your Computer)
+  db_conn <- dbConnect(
+    RPostgres::Postgres(),
+    dbname   = "healthcenter",
+    host     = "localhost",
+    port     = 5432,
+    user     = "postgres",
+    password = "your_local_password"
+  )
+}
