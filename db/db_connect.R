8eaab851d412800cac1ff8439d9d5952b8bb6e7b
Seph Solomo <solomojoseph051@gmail.com>
2026-01-12 21:56:24 +0800

    Final database connection logic fix

--- db/db_connect.R
+++ db/db_connect.R
@@ -1,27 +1,30 @@
 library(DBI)
 library(RPostgres)
 
-# Check if we are on Render by looking for a Render-specific variable
 is_render <- Sys.getenv("RENDER") != ""
 
 if (is_render) {
-  # RENDER CONNECTION (Cloud)
   db_conn <- dbConnect(
     RPostgres::Postgres(),
     dbname   = Sys.getenv("DB_NAME"),
     host     = Sys.getenv("DB_HOST"),
-    port     = as.integer(Sys.getenv("DB_PORT")),
+    port     = as.integer(Sys.getenv("DB_PORT", "5432")), 
     user     = Sys.getenv("DB_USER"),
     password = Sys.getenv("DB_PASS")
   )
 } else {
-  # LOCAL CONNECTION (Your Computer)
   db_conn <- dbConnect(
     RPostgres::Postgres(),
     dbname   = "healthcenter",
     host     = "localhost",
     port     = 5432,
     user     = "postgres",
-    password = "your_local_password"
+    password = "your_local_password" # Change this to your local pgAdmin password
   )
+}
+
+if (dbIsValid(db_conn)) {
+  message("SUCCESS: Connected to the database.")
+} else {
+  stop("ERROR: Could not establish a database connection.")
 }
