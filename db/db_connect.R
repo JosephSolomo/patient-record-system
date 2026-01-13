51ac3e5799ebe909aeee877ec8f312ac016d2c8f
Seph Solomo <solomojoseph051@gmail.com>
2026-01-13 11:39:30 +0800

    Fix database connection function and server logic

--- db/db_connect.R
+++ db/db_connect.R
@@ -1,30 +1,37 @@
 library(DBI)
 library(RPostgres)
 
-is_render <- Sys.getenv("RENDER") != ""
-
-if (is_render) {
-  db_conn <- dbConnect(
-    RPostgres::Postgres(),
-    dbname   = Sys.getenv("DB_NAME"),
-    host     = Sys.getenv("DB_HOST"),
-    port     = as.integer(Sys.getenv("DB_PORT", "5432")), 
-    user     = Sys.getenv("DB_USER"),
-    password = Sys.getenv("DB_PASS")
-  )
-} else {
-  db_conn <- dbConnect(
-    RPostgres::Postgres(),
-    dbname   = "healthcenter",
-    host     = "localhost",
-    port     = 5432,
-    user     = "postgres",
-    password = "your_local_password" # Change this to your local pgAdmin password
-  )
+create_connection <- function() {
+  is_render <- Sys.getenv("RENDER") != ""
+  
+  if (is_render) {
+    # Cloud Connection (Render)
+    dbConnect(
+      RPostgres::Postgres(),
+      dbname   = Sys.getenv("DB_NAME"),
+      host     = Sys.getenv("DB_HOST"),
+      port     = as.integer(Sys.getenv("DB_PORT", "5432")), 
+      user     = Sys.getenv("DB_USER"),
+      password = Sys.getenv("DB_PASS")
+    )
+  } else {
+    # Local Connection
+    dbConnect(
+      RPostgres::Postgres(),
+      dbname   = "healthcenter",
+      host     = "localhost",
+      port     = 5432,
+      user     = "postgres",
+      password = "your_local_password" # Change this to your local pgAdmin password
+    )
+  }
 }
 
-if (dbIsValid(db_conn)) {
-  message("SUCCESS: Connected to the database.")
-} else {
-  stop("ERROR: Could not establish a database connection.")
-}
+tryCatch({
+  db_conn <- create_connection()
+  if (dbIsValid(db_conn)) {
+    message("SUCCESS: Connected to the database.")
+  }
+}, error = function(e) {
+  message("ERROR: Could not establish a database connection: ", e$message)
+})
