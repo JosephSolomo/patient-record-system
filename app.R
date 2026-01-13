966dca22880780c6a5291878bd233c8ca7139d93
Seph Solomo <solomojoseph051@gmail.com>
2026-01-13 15:35:23 +0800

    Update app.R with correct module paths

--- app.R
+++ app.R
@@ -2,7 +2,14 @@
 # app.R
 # ================================
 
-source("patient_map.R")
+# Load required libraries
+library(shiny)
+library(DBI)
+library(RPostgres)
+
+source("module/patient_map.R")
+source("module/patient_form.R") # Sourcing this as well since it's in your module folder
+
 source("global.R")
 
 source("ui/ui_main.R")
