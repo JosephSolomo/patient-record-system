e05440b19ce776c7e426b75b71aedf2efd85476a
Seph Solomo <solomojoseph051@gmail.com>
2026-01-14 01:23:27 +0800

    Fix: removed old module source paths to use R folder

--- app.R
+++ app.R
@@ -5,12 +5,10 @@ library(shiny)
 library(DBI)
 library(RPostgres)
 
-source("module/patient_map.R", local = TRUE)
-source("module/patient_form.R", local = TRUE)
 
-source("global.R", local = TRUE)
+source("global.R")
 
-source("ui/ui_main.R", local = TRUE)
-source("server/server_main.R", local = TRUE)
+source("ui/ui_main.R")
+source("server/server_main.R")
 
 shinyApp(ui, server)
